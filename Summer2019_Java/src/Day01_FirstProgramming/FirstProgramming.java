9a9125fa2f392bec5ed21ecef0b8d52782f8e38b
RuhanBoz <55070332+RuhanBoz@users.noreply.github.com>
2019-09-08 22:40:32 -0500

    Update FirstProgramming.java

--- Summer2019_Java/src/Day01_FirstProgramming/FirstProgramming.java
+++ Summer2019_Java/src/Day01_FirstProgramming/FirstProgramming.java
@@ -3,7 +3,7 @@ package Day01_FirstProgramming;
 public class FirstProgramming {
 
 	public static void main(String[] args) {
-	
+	:// NEW UPDATES
 		   System.out.println("Hello github!");
 		   
 	
