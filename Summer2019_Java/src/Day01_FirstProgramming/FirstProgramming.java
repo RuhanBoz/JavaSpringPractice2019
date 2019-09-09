e75cb9ec6c739147ed81264505385d4085a587f2
Ruhan Boz <ruhan.yunus@gmail.com>
2019-09-08 22:32:14 -0500

    hello git!

--- Summer2019_Java/src/Day01_FirstProgramming/FirstProgramming.java
+++ Summer2019_Java/src/Day01_FirstProgramming/FirstProgramming.java
@@ -4,14 +4,8 @@ public class FirstProgramming {
 
 	public static void main(String[] args) {
 	
-		   System.out.println("Hello World");
-		   System.out.println("Hello World");
-			
-		   System.out.println("Hello World");
-
-		   System.out.println("Hello World");
-	
-	
+		   System.out.println("Hello github!");
+		   
 	
 	}
 
